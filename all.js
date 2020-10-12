7c9cddef49a525f93717d2d2bbd58ec5a34a3076
qq860831 <qq860831@gmail.com>
2020-10-12 16:21:17 +0800

    123

--- all.js
+++ all.js
@@ -1,8 +1,34 @@
 $(document).ready(function(){
-    $('.dropdown').click(function(event){
-        event.preventDefault();
-        // 取消事件默認行為
-        $(this).parent().siblings().find('.dropdown-open').slideUp();
-        $(this).parent().find('.dropdown-open').slideToggle();
-    });
+    // $('.dropdown').click(function(event){
+    //     event.preventDefault();
+    //     // 取消事件默認行為
+    //     $(this).parent().siblings().find('.dropdown-open').slideUp();
+    //     $(this).parent().find('.dropdown-open').slideToggle();
+    // });
+
+    // 輪播效果
+    var mySwiper = new Swiper ('.swiper-container', {
+        direction: 'horizontal', // 垂直切换选项
+        loop: true, // 循环模式选项
+        effect : 'fade',
+        fadeEffect: {
+        crossFade: true,
+        },
+        autoplay: true,
+        // 如果需要分页器
+        pagination: {
+            el: '.swiper-pagination',
+          },
+        
+        // 如果需要前进后退按钮
+        navigation: {
+          nextEl: '.swiper-button-next',
+          prevEl: '.swiper-button-prev',
+        },
+        
+        // 如果需要滚动条
+        scrollbar: {
+          el: '.swiper-scrollbar',
+        },
+      })
 });
