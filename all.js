213b86721a2fd14734cced80c37a535560308a88
qq860831 <qq860831@gmail.com>
2020-10-13 09:35:36 +0800

    123

--- all.js
+++ all.js
@@ -8,27 +8,27 @@ $(document).ready(function(){
 
     // 輪播效果
     var mySwiper = new Swiper ('.swiper-container', {
-        direction: 'horizontal', // 垂直切换选项
-        loop: true, // 循环模式选项
+        direction: 'horizontal', 
+        loop: true, 
         effect : 'fade',
         fadeEffect: {
         crossFade: true,
         },
         autoplay: true,
-        // 如果需要分页器
+        
         pagination: {
             el: '.swiper-pagination',
           },
         
-        // 如果需要前进后退按钮
+       
         navigation: {
           nextEl: '.swiper-button-next',
           prevEl: '.swiper-button-prev',
         },
         
-        // 如果需要滚动条
+        
         scrollbar: {
           el: '.swiper-scrollbar',
         },
-      })
+    })
 });
