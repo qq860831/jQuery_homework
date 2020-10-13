d55d3a1aca4c4ab5f7f7233df8d7e8575ce46de3
qq860831 <qq860831@gmail.com>
2020-10-13 14:11:52 +0800

    123

--- all.js
+++ all.js
@@ -1,52 +1,46 @@
-$(document).ready(function(){
-
-    $('.dropdown').click(function(e){ 
-        e.preventDefault();
-        $(this).parent().siblings().find('.dropdown-open').slideUp();
-        $(this).parent().find('.dropdown-open').slideToggle();
-        // 點擊到這個元素回到父層再到父層內找子元素 .dropdown-open進行 slideToggle 這動作
-    });
-
-    // 燈箱(圖不大效果不明顯)
-    lightbox.option({
-        'resizeDuration':3000,
-        'wrapAround':true,
-        'fadeDuration':1000,
-        'imageFadeDuration':1000,
-    });
-
-    // go-top
-    $('.icon').click(function(e){
-        e.preventDefault();
-        $('html,body').animate({
-            scrollTop: 0
-        },700);
-    });
-
+$(document).ready(function () {
+    // 收合選單
+  $('.dropdown').click(function(e){ 
+    e.preventDefault();
+    $(this).parent().siblings().find('.dropdown-open').slideUp();
+    $(this).parent().find('.dropdown-open').slideToggle();
+    // 點擊到這個元素回到父層再到父層內找子元素 .dropdown-open進行 slideToggle 這動作
+  });
+  });
+  
     // 輪播效果
     var mySwiper = new Swiper ('.swiper-container', {
-        direction: 'horizontal', 
-        loop: true, 
-        effect : 'fade',
-        fadeEffect: {
-        crossFade: true,
+      direction: 'horizontal', // 垂直切换选项
+      loop: true, // 循环模式选项
+      effect : 'fade',
+      fadeEffect: {
+      crossFade: true,
+      },
+      autoplay: true,
+      // 如果需要分页器
+      pagination: {
+          el: '.swiper-pagination',
         },
-        autoplay: true,
-        
-        pagination: {
-            el: '.swiper-pagination',
-          },
-        
-       
-        navigation: {
-          nextEl: '.swiper-button-next',
-          prevEl: '.swiper-button-prev',
-        },
-        
-        
-        scrollbar: {
-          el: '.swiper-scrollbar',
-        },
+      
+      // 如果需要前进后退按钮
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+      
+      // 如果需要滚动条
+      scrollbar: {
+        el: '.swiper-scrollbar',
+      },
+    })      
+  
+  
+    //回到最上方效果
+    // 在屬性名稱上設置一個點擊事件，事件內用scrollTop語法
+    $('.fa-chevron-circle-up').click(function(e){
+        e.preventDefault();
+        $('html,body').animate({
+          scrollTop: 0
+        },700);
     });
-
-});
+  
