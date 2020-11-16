4d1c54708cd86a4aec6421f69b47bd3e71c64286
Ian <qq860831@gmail.com>
2020-11-16 17:05:40 +0800

    finish

--- all.js
+++ all.js
@@ -6,36 +6,38 @@ $(document).ready(function () {
     $(this).parent().find('.dropdown-open').slideToggle();
     // 點擊到這個元素回到父層再到父層內找子元素 .dropdown-open進行 slideToggle 這動作
   });
+  // 輪播效果
+      var swiper = new Swiper('.swiper-container', {
+        cssMode: true,
+        autoHeight: true, //高度一樣
+        loop:true,  //循環播放 
+        slidesPerView : 'auto',
+        navigation: {
+          nextEl: '.swiper-button-next',
+          prevEl: '.swiper-button-prev',
+        },
+        pagination: {
+          el: '.swiper-pagination'
+        },
+        mousewheel: false,
+        keyboard: true,
+
+        autoplay: {
+          delay: 3000,
+        },
+      });     
+    
+    
+      //回到最上方效果
+      // 在屬性名稱上設置一個點擊事件，事件內用scrollTop語法
+      $('.fa-chevron-circle-up').click(function(e){
+          e.preventDefault();
+          $('html,body').animate({
+            scrollTop: 0
+          },1000);
+      });
+
   });
   
-    // 輪播效果
-    var swiper = new Swiper('.swiper-container', {
-      cssMode: true,
-      autoHeight: true, //高度一樣
-      loop:true,  //循環播放 
-      slidesPerView : 'auto',
-      navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-      },
-      pagination: {
-        el: '.swiper-pagination'
-      },
-      mousewheel: false,
-      keyboard: true,
-
-      autoplay: {
-        delay: 3000,
-      },
-    });     
-  
-  
-    //回到最上方效果
-    // 在屬性名稱上設置一個點擊事件，事件內用scrollTop語法
-    $('.fa-chevron-circle-up').click(function(e){
-        e.preventDefault();
-        $('html,body').animate({
-          scrollTop: 0
-        },1000);
-    });
+    
   
